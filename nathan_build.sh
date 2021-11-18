1e40e4c97590b7bba813e900f8d612830f78304f
Nathan O <Nathan O>
2021-11-18 13:17:44 -0500

    Re-add build script

---
+++ nathan_build.sh
@@ -0,0 +1,11 @@
+# A quick script so I can remember my build command.
+
+export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
+export BUILD_CAFFE2=0
+export BUILD_CAFFE2_OPS=0
+export BUILD_TEST=0
+export MAX_JOBS=4
+
+# Run setup.py, redirecting output to build_log.txt for later review if needed.
+stdbuf -oL python setup.py install 2>&1 | tee build_log.txt
+
